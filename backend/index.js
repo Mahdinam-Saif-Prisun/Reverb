c952eaf8032fa46df00d34b222a2d8ffd68ba3a7
Mahdinam-Saif-Prisun <m.saif.prisun@gmail.com>
2025-08-23 22:30:01 +0600

    Backend server up

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get("/test", (req, res) => {
+  res.send("Backend is working!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
